0f5f6a22ab1688bf8565b6c2c821b9f3bd3b162d
trommejarl <95622359+trommejarl@users.noreply.github.com>
2022-03-11 09:33:59 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,53 @@
+//*Knap kopier mørkeblå*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f2'));
+
+  alert("Hex kode kopiret")
+}
+
+//*Knap kopier lyseblå*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn-f3').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f3'));
+
+  alert("Hex kode kopiret")
+}
---
+++ java0.js
@@ -0,0 +1,3 @@
+console.log("Det her er en lille test");
+document.body.innerHTML=("<h1>Dette er en ekstern js fil</h1>");
+document.getElementById("test").innerHTML =("Nu kører det")
