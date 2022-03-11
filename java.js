fa93c773ef8907e2b0a023851063b6a919df4b5a
trommejarl <ejkm38523@edu.ucl.dk>
2022-03-11 12:22:34 +0100

    hh

--- java.js
+++ java.js
@@ -51,3 +51,114 @@ function ()
 
   alert("Hex kode kopiret")
 }
+
+
+//*Knap kopier lyserød*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn-f4').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f4'));
+
+  alert("Hex kode kopiret")
+}
+
+//*Knap kopier lysegul*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn-f5').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f5'));
+
+  alert("Hex kode kopiret")
+}
+
+
+//*Knap kopier rød*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn-f6').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f6'));
+
+  alert("Hex kode kopiret")
+}
+
+
+//*Knap kopier hvid*//
+
+function copyText(htmlElement)
+{
+  if (!htmlElement){
+    return;
+}
+
+let elementText = htmlElement.innerText;
+
+let inputElement = document.createElement('input');
+inputElement.setAttribute('value', elementText);
+document.body.appendChild(inputElement);
+inputElement.select();
+document.execCommand('copy');
+inputElement.parentNode.removeChild(inputElement);
+
+}
+
+document.querySelector('#copy-text-btn-f7').onclick =
+function ()
+{
+  copyText(document.querySelector('#text-f7'));
+
+  alert("Hex kode kopiret")
+}
